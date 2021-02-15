02c0da7883939b141fa7b0c251a3e0f85d97b7cb
Aditya Rai <adityarai2109@gmail.com>
2021-02-15 14:43:09 +0530

    Implemented fetching using Axios

---
+++ src/FetchAuthor.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function FetchAuthor(props) {
+  const [author, setAuthor] = useState();
+  const fetchData = () => {
+    axios
+      .get(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
+      .then((response) => {
+        setAuthor(response.data.quotes[props.i].author);
+      });
+  };
+  fetchData();
+
+  return <div>{author}</div>;
+}
